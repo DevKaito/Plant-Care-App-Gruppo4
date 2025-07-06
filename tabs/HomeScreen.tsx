1feea941e7b3c5a2735794d288d80910f7c34022
LiLDak777 <m.dacunto18@studenti.unisa.it>
2025-07-06 17:18:40 +0200

    Miglioramento CSS HomeScreen.tsx

--- tabs/HomeScreen.tsx
+++ tabs/HomeScreen.tsx
@@ -12,19 +12,25 @@ import {
 type Plant = {
     id: string;
     name: string;
+    image: string;
 };
 
 const dummyPlants: Plant[] = [
-    { id: 'p1', name: 'Nome p1' },
-    { id: 'p2', name: 'Nome p2' },
-    { id: 'p3', name: 'Nome p3' },
+    { id: 'p1', name: 'Mimosa sensitiva', image: 'https://cdn-icons-png.flaticon.com/128/3800/3800257.png' },
+    { id: 'p2', name: 'Nome p2', image: 'https://cdn-icons-png.flaticon.com/128/3800/3800257.png' },
+    { id: 'p3', name: 'Nome p3', image: 'https://cdn-icons-png.flaticon.com/128/3800/3800257.png' },
+    { id: 'p4', name: 'Nome p3', image: 'https://cdn-icons-png.flaticon.com/128/3800/3800257.png' },
+    { id: 'p5', name: 'Nome p3', image: 'https://cdn-icons-png.flaticon.com/128/3800/3800257.png' },
 ];
 
 const HomeScreen: React.FC = () => {
     const renderPlantItem = ({ item }: { item: Plant }) => (
         <View style={styles.row}>
             <View style={styles.idBox}>
-                <Text style={styles.idText}>{item.id}</Text>
+                <Image
+                        source={{ uri: item.image }}
+                        style={styles.boxImage}
+                    />
             </View>
             <View style={styles.nameBox}>
                 <Text style={styles.nameText}>{item.name}</Text>
@@ -44,21 +50,21 @@ const HomeScreen: React.FC = () => {
             <View style={styles.categoryRow}>
                 <View style={styles.categoryItem}>
                     <Image
-                        source={{ uri: 'https://via.placeholder.com/60?text=Cactus' }}
+                        source={{ uri: 'https://cdn-icons-png.flaticon.com/128/882/882968.png' }}
                         style={styles.categoryImage}
                     />
                     <Text style={styles.categoryText}>Cactus</Text>
                 </View>
                 <View style={styles.categoryItem}>
                     <Image
-                        source={{ uri: 'https://via.placeholder.com/60?text=Palma' }}
+                        source={{ uri: 'https://cdn-icons-png.flaticon.com/128/2220/2220105.png' }}
                         style={styles.categoryImage}
                     />
                     <Text style={styles.categoryText}>Palma</Text>
                 </View>
                 <View style={styles.categoryItem}>
                     <Image
-                        source={{ uri: 'https://via.placeholder.com/60?text=Bonsai' }}
+                        source={{ uri: 'https://cdn-icons-png.flaticon.com/128/2362/2362691.png' }}
                         style={styles.categoryImage}
                     />
                     <Text style={styles.categoryText}>Bonsai</Text>
@@ -82,20 +88,20 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingHorizontal: 16,
-        backgroundColor: '#fff',
+        backgroundColor: 'white',
     },
     header: {
-        fontSize: 22,
+        fontSize: 42,
         fontWeight: 'bold',
         textAlign: 'center',
-        marginTop: 10,
-        marginBottom: 12,
+        marginTop: 24,
     },
     sectionTitle: {
-        fontSize: 16,
+        fontSize: 20,
         fontWeight: '600',
         textAlign: 'center',
-        marginVertical: 8,
+        marginTop: 48,
+        marginBottom: 32,
     },
     categoryRow: {
         flexDirection: 'row',
@@ -104,21 +110,28 @@ const styles = StyleSheet.create({
     },
     categoryItem: {
         alignItems: 'center',
+        marginBottom: 24,
     },
     categoryImage: {
-        width: 60,
-        height: 60,
+        width: 64,
+        height: 64,
     },
     categoryText: {
         marginTop: 4,
-        fontSize: 12,
+        fontSize: 16,
+        fontStyle: 'italic',
     },
     boxContainer: {
-        borderWidth: 2,
-        borderColor: '#000',
-        borderRadius: 8,
-        padding: 8,
-        marginBottom: 80,
+        borderWidth: 3,
+        borderColor: 'black',
+        borderRadius: 16,
+        backgroundColor: '#cffcd2',
+        paddingLeft: 8,
+        paddingTop: 8,
+        paddingBottom: 8,
+        marginRight: 30,
+        marginLeft: 30,
+        maxHeight: 320,
     },
     row: {
         flexDirection: 'row',
@@ -126,31 +139,36 @@ const styles = StyleSheet.create({
         marginBottom: 10,
     },
     idBox: {
-        backgroundColor: '#4CAF50',
+        backgroundColor: 'white',
         padding: 10,
-        borderRadius: 4,
+        borderRadius: 16,
+        borderWidth: 2,
+        borderColor: 'black',
         marginRight: 10,
-    },
-    idText: {
-        color: 'white',
-        fontWeight: 'bold',
     },
     nameBox: {
         flex: 1,
     },
     nameText: {
-        fontSize: 16,
+        fontSize: 17,
+        fontWeight: '500',
     },
     curaButton: {
         backgroundColor: '#4CAF50',
         paddingVertical: 6,
         paddingHorizontal: 12,
         borderRadius: 20,
+        marginRight: 12,
     },
     curaText: {
-        color: '#fff',
+        color: 'white',
         fontWeight: 'bold',
+        fontSize: 20,
     },
+    boxImage: {
+        width: 48,
+        height: 48,
+    }
 });
 
 
