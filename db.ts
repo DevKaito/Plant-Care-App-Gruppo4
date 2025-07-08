1bdb11090bdf980e5674b2782273c2cd41cbed90
Kaito <micheledungeon@gmail.com>
2025-07-08 16:45:09 +0200

    Database update

--- db.ts
+++ db.ts
@@ -54,31 +54,31 @@ export const deleteTable = async (db: SQLite.SQLiteDatabase) => {
 
 export const getPlants = async (db:SQLite.SQLiteDatabase): Promise<Plant[]> => {
     if(db){
-    const selectQuery = 'SELECT * FROM plants';
-    const results = await db.getAllAsync(selectQuery);
-    const plants: Plant[] = [];
+        const selectQuery = 'SELECT * FROM plants';
+        const results = await db.getAllAsync(selectQuery);
+        const plants: Plant[] = [];
     
-    if(results.length < 1) {
+        if(results.length < 1) {
+            return plants;
+        }
+
+        results.map((row: any) => {
+            const plant: Plant = {
+                key: row.id,
+                name: row.name,
+                species: row.species,
+                ownedSince: new Date(row.acquireDate),
+                waterFrequency: row.waterFreq,
+                repotFrequency: row.repotFreq,
+                pruneFrequency: row.pruneFreq,
+                image: row.image ? row.image : '../assets/placeholder.png',
+                state: row.status ? row.status as PlantState : PlantState.Healthy
+            };
+            plants.push(plant);
+        })
+        console.log(plants);
         return plants;
     }
-
-    results.map((row: any) => {
-        const plant: Plant = {
-            key: row.id,
-            name: row.name,
-            species: row.species,
-            ownedSince: new Date(row.acquireDate),
-            waterFrequency: row.waterFreq,
-            repotFrequency: row.repotFreq,
-            pruneFrequency: row.pruneFreq,
-            image: row.image ? row.image : '../assets/placeholder.png',
-            state: row.status ? row.status as PlantState : PlantState.Healthy
-        };
-        plants.push(plant);
-    })
-    console.log(plants);
-    return plants;
-}
     else{
         throw new Error('Database connection is not established');
     }
